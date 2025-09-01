1e2640ad97b6458c5f9a38fdc9f3529ac39d615e
Simon Verhoeven <verhoeven.simon@gmail.com>
2025-09-01 18:57:26 +0200

    feature: bootstrap project

---
+++ src/test/java/dev/simonverhoeven/springframework7demo/SpringFramework7DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.simonverhoeven.springframework7demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFramework7DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
