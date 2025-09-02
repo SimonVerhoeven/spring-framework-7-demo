340544cfde13fbc8e8f9f6b0e6ca42d301f5d40c
Simon Verhoeven <verhoeven.simon@gmail.com>
2025-09-02 18:48:38 +0200

    chore: update retryable example

--- src/main/java/dev/simonverhoeven/springframework7demo/ResilientService.java
+++ src/main/java/dev/simonverhoeven/springframework7demo/ResilientService.java
@@ -4,11 +4,22 @@ import org.springframework.resilience.annotation.ConcurrencyLimit;
 import org.springframework.resilience.annotation.Retryable;
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 @Service
 public class ResilientService {
 
-    @Retryable(maxAttempts = 5L, jitter = 10L)
+    @Retryable(
+            maxAttempts = 5L,
+            jitter = 10L,
+            multiplier = 2L
+    )
     public void retryable() {
+        if (50 < ThreadLocalRandom.current().nextInt(0, 100)) {
+            throw new RuntimeException("Random failure");
+        } else {
+            System.out.println("Random success");
+        }
     }
 
     @ConcurrencyLimit(value = 5)
