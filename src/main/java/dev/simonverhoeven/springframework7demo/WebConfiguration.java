12d54080acc36c8c9b4c4144a24e2041a213607a
Simon Verhoeven <verhoeven.simon@gmail.com>
2025-09-09 18:13:53 +0200

    feature: start adding message converters

--- src/main/java/dev/simonverhoeven/springframework7demo/WebConfiguration.java
+++ src/main/java/dev/simonverhoeven/springframework7demo/WebConfiguration.java
@@ -1,8 +1,15 @@
 package dev.simonverhoeven.springframework7demo;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.json.JsonMapper;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverters;
+import org.springframework.http.converter.json.JacksonJsonHttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ApiVersionConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.text.SimpleDateFormat;
 
 @Configuration
 public class WebConfiguration implements WebMvcConfigurer {
@@ -12,5 +19,15 @@ public class WebConfiguration implements WebMvcConfigurer {
         configurer
                 .useRequestHeader("X-Api-Version")
                 .setVersionRequired(true);
+    }
+
+    @Override
+    public void configureMessageConverters(HttpMessageConverters.ServerBuilder builder) {
+        final ObjectMapper jsonMapper = JsonMapper.builder()
+                .findAndAddModules()
+                .enable(SerializationFeature.INDENT_OUTPUT)
+                .defaultDateFormat(new SimpleDateFormat("dd-MM-yyyy"))
+                .build();
+//        builder.jsonMessageConverter(new JacksonJsonHttpMessageConverter(jsonMapper));
     }
 }
